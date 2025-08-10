96c80370202901a44949a8185fe0226f0a84daa9
Maoz <110767210+LeeSkies@users.noreply.github.com>
2025-08-10 10:34:44 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ import { registerPlugin } from '@capacitor/core';
 import type { BroadcastReceiverPlugin } from './definitions';
 
 const BroadcastReceiver = registerPlugin<BroadcastReceiverPlugin>(
-  'BroadcastReceiver',
-  {
-    web: () => console.error('BroadcastReceiver is not available on web'),
-  },
+  'BroadcastReceiver'
 );
 
 export * from './definitions';
